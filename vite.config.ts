2d81ad3291b27e659e08d06319c573ce9c9634e3
Cannyster <102709384+Cannyster@users.noreply.github.com>
2024-09-03 11:32:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-tood-list",
-  plugins: [react()],
-  server: {
-    port: 3001,
-  },
+  base: "/TO-DO-List/",
+  plugins: [react()]
 });
